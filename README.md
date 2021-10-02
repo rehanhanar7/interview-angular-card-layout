0fa34aef8d38e9ebf7f6aa45d13d8587ba19c163
rehanhanar7 <rehanhanar7@gmail.com>
2021-10-02 12:07:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-a8t9fk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-a8t9fk)
---
+++ src/app/card-overview-example.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+/**
+ * @title Basic cards
+ */
+@Component({
+  selector: 'card-overview-example',
+  templateUrl: 'card-overview-example.html',
+  styles: [
+    `.main-div {
+    margin-bottom: 1vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  `,
+  ],
+})
+export class CardOverviewExample {}
+
+/**  Copyright 2021 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
